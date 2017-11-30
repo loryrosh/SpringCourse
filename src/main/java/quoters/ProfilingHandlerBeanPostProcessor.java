149bf9b7549b6876da66cb85e3fc526393c074b0
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-11-30 23:07:16 +0300

    [SpringCourse] Add profiling injection

--- src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
@@ -31,7 +31,17 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
                     beanClass.getInterfaces(), new InvocationHandler() {
                         @Override
                         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                            return null;
+                            if (controller.isEnabled()) {
+                                System.out.println("Профилирую");
+                                long before = System.nanoTime();
+                                Object res = method.invoke(bean, args);
+                                long after = System.nanoTime();
+                                System.out.println(after - before);
+                                System.out.println("Всё");
+                                return res;
+                            } else {
+                                return method.invoke(bean, args);
+                            }
                         }
                     });
         }
