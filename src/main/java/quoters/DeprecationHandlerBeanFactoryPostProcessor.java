cf6a083d383e8521d44df79edfe4b518b0deb1ae
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-12-01 11:54:25 +0300

    [SpringCourse] Add PostProcessor to handle @DeprecatedClass

---
+++ src/main/java/quoters/DeprecationHandlerBeanFactoryPostProcessor.java
@@ -0,0 +1,15 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class DeprecationHandlerBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        String[] names = beanFactory.getBeanDefinitionNames();
+        for (String name : names) {
+            beanFactory.getBeanDefinition(name);
+        }
+    }
+}
