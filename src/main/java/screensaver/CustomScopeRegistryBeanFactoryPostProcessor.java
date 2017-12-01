9044cdefb27a17104895fe0b68d1685d32f6d300
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-12-01 13:04:53 +0300

    [SpringCourse] Add PostProcessor for "periodical" scope

---
+++ src/main/java/screensaver/CustomScopeRegistryBeanFactoryPostProcessor.java
@@ -0,0 +1,12 @@
+package screensaver;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class CustomScopeRegistryBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        beanFactory.registerScope("periodical", new PeriodicalScopeConfigurer());
+    }
+}
